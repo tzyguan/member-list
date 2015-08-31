b5a25750d1efa63e0a986dccbb57dd1486245944
tzyg <tg@aol.com>
2015-08-30 17:17:24 -0700

    submit via pull request

--- src/script.js
+++ src/script.js
@@ -1,1 +1,54 @@
-angular.module('app.memberList', []);
+//angular.module('app.memberList', []);
+
+var app = angular.module('app.memberList', []); 
+app.controller('memberCtrl', function($scope) {
+
+   // member List Arrays
+	$scope.membersList = [];
+
+		var i = 0;
+		function id(){
+			return i = i+1;
+		
+		}
+		$scope.membersList = [
+			{
+				arry_id: id(),
+				arry_name:'teeg',
+				arry_email:'tg@aol.com',
+				arry_tele:'017-9552153',
+				arry_date:new Date().toUTCString(),
+				arry_done:false
+			}
+		
+		];
+		//console.log($scope.membersList[0].email);
+		
+	
+		$scope.todoAdd = function() {
+			
+			 if($scope.Name.length == 0 || $scope.Email.length == 0 || $scope.Tel.length == 0) {alert("Error");return;}
+			 
+			  $scope.membersList.unshift({
+				arry_id: id(),
+				arry_name: $scope.Name,
+				arry_email : $scope.Email,
+				arry_tele: $scope.Tel,
+				arry_date:  new Date().toUTCString(),
+				arry_done:false
+				
+			});
+			$scope.Name="";
+			$scope.Email="";
+			$scope.Tel ="";
+		};
+
+	
+		$scope.remove = function() {
+		var oldmembers = $scope.membersList;
+		$scope.membersList = [];
+			angular.forEach(oldmembers, function(members) {
+				if (!members.arry_done) $scope.membersList.push(members);
+			});
+		};
+});
--- src/test/script.spec.js
+++ src/test/script.spec.js
@@ -3,3 +3,86 @@ describe('script.js Tests', function() {
         expect(true).toBeTruthy();
     });
 });
+
+
+describe('MemberList Test', function(){
+  beforeEach(module('app.memberList'));
+
+  var $controller;
+
+  beforeEach(inject(function(_$controller_){
+    // The injector unwraps the underscores (_) from around the parameter names when matching
+    $controller = _$controller_;
+  }));
+  
+	//check array
+	describe('Read member Test', function(){
+		it('Read from array', function(){
+		
+			var $scope = {};
+			var controller = $controller('memberCtrl', { $scope: $scope });
+			expect($scope.membersList[0].arry_name).toEqual('teeg');
+		});
+	});
+  
+	//add null test
+	describe('Add null Test', function(){
+		it('add null into array', function(){
+		
+			var $scope = {};
+			var controller = $controller('memberCtrl', { $scope: $scope });
+
+			$scope.Name = '';
+			$scope.Email = '';
+			$scope.Tel = '';
+		
+			$scope.todoAdd();
+			console.log($scope.todoAdd());
+			expect($scope.todoAdd()).toEqual();
+		});
+	});
+	
+	//add test
+	describe('Add member Test', function(){
+		it('add into array', function(){
+		
+			var $scope = {};
+			var controller = $controller('memberCtrl', { $scope: $scope });
+
+			$scope.Name = 'Test';
+			$scope.Email = 'test@mail.com';
+			$scope.Tel = '017-9552153';
+
+			console.log('before add - Name = '+ $scope.membersList[0].arry_name);
+			$scope.todoAdd();
+			console.log('after add - Name = '+ $scope.membersList[0].arry_name);
+
+			expect($scope.membersList[0].arry_name).toEqual('Test');
+		});
+	});
+	
+		//delete test
+	describe('Delete member Test', function(){
+		it('remove from array', function(){
+
+		var $scope = {};
+		var controller = $controller('memberCtrl', { $scope: $scope });
+			
+			
+		$scope.Name = 'Test1';
+		$scope.Email = 'test@mail.com';
+		$scope.Tel = '017-9552153';
+		$scope.todoAdd();
+		console.log('after add - Name = '+ $scope.membersList[0].arry_name);	
+			
+		$scope.membersList[0].arry_done = true;
+		console.log('before del - Name = '+ $scope.membersList[0].arry_name);
+		
+		$scope.remove();
+		console.log('after del - Name = '+ $scope.membersList[0].arry_name);
+		expect($scope.membersList[0].arry_name).toEqual('teeg'); 
+
+		});
+	});
+
+});
